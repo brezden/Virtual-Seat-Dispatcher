78bcf6eb9c3e9ef6d007f8ccf53fd56f65265f8b
Callum Brezden <callumbrezden@gmail.com>
2023-11-30 14:54:19 -0800

    Sign In Working

--- src/app/_components/buttons/sign-in.tsx
+++ src/app/_components/buttons/sign-in.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import MicrosoftIcon from "@mui/icons-material/Microsoft";
+import { signIn } from "next-auth/react"
 
 export default function SignInButton() {
   return (
@@ -6,6 +9,7 @@ export default function SignInButton() {
       <button
         type="button"
         className="bg-highlight text-primary hover:bg-highlight_hover focus-visible:outline-highlight inline-flex items-center gap-x-3 rounded-lg px-5 py-3 text-lg font-bold shadow-lg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 transition duration-300 ease-in-out"
+        onClick={() => signIn()}
       >
         <MicrosoftIcon className="h-6 w-6" aria-hidden="true" />
         Sign in with Microsoft
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import { getServerSession } from "next-auth/next"
 import SignInButton  from 'src/app/_components/buttons/sign-in';
 
 export default async function Home() {
+  const session = await getServerSession();
+  console.log(session);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-background to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem] text-primary">
           Virtual Seat Dispatcher
         </h1>
-        <SignInButton />
+        {session ? (
+          <div>
+            <p>Hi {session?.user?.name}!</p>
+          </div>
+        ) : (
+          <SignInButton />
+        )}
       </div>
     </main>
   );
