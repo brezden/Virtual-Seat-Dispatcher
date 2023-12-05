c0d49b33318dfb4c61ce3e3ef2db92454299e6c4
Callum Brezden <callumbrezden@gmail.com>
2023-12-05 15:55:44 -0800

    Handler Fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,6 @@ import { options } from "./options";
 const handler = NextAuth(options) as NextAuthOptions;
 
 // Named exports for each HTTP method (GET and POST)
-export { handler as default };
+export default handler;
 export { handler as GET };
 export { handler as POST };
