0df8f0109ce552f86ef7afe6277f6a7b862a0321
Callum Brezden <callumbrezden@gmail.com>
2023-12-20 14:25:52 -0800

    Added Delete Query

--- src/app/_components/buttons/delete-booking.tsx
+++ src/app/_components/buttons/delete-booking.tsx
@@ -1,16 +1,24 @@
 "use client";
 
 import { TrashIcon } from "@heroicons/react/20/solid";
+import { api } from "~/trpc/react";
+import { useRouter } from "next/navigation";
 
 interface DeleteBookingProps {
   bookingId: number;
 }
 
 export default function DeleteBooking({ bookingId }: DeleteBookingProps) {
+  const router = useRouter();
+
+  const { mutate } = api.meeting.deleteBooking.useMutation({
+    onSuccess: () => {
+      router.refresh();
+    },
+  });
+
   const handleDelete = () => {
-    // Implement the delete logic here
-    console.log(`Deleting booking with ID: ${bookingId}`);
-    // For example, you could call an API to delete the booking
+    mutate({ bookingId });
   };
 
   return (
@@ -18,7 +26,7 @@ export default function DeleteBooking({ bookingId }: DeleteBookingProps) {
       <div className="flex justify-center items-center">
         <button 
           onClick={handleDelete} 
-          className="flex justify-center items-center p-2 hover:bg-red-500 rounded transition-colors duration-300" // Adjusted padding and added transition
+          className="flex justify-center items-center p-2 hover:bg-red-500 rounded transition-colors duration-300"
         >
           <TrashIcon 
             className="h-5 w-5 text-white lg:h-6 lg:w-6" 
